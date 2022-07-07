709a86c8452c52c070a2936ec8b4302deda5a5b2
Leonardo Aucar <leonardoaucar@gmail.com>
2022-07-07 09:49:03 -0300

    atualização do desenho da base

--- programa.py
+++ programa.py
@@ -18,10 +18,33 @@ print(pageObj.extractText())
 # closing the pdf file object 
 pdfFileObj.close()
 
-#relatório
-#ano
-#sessão
-#pagina
-#texto (por parágrafo?)
-#paragrafo
+
+#loop for vai passar relatório por relatório
+#em cada relatório vai tentar extrair todas as páginas
+#vai adicionar linhas num grande DF (abaixo colunas)
+#ao fim exportaremos o CSV
+
 #id
+#texto (como vou separar títulos? e tabelas?)
+#documento/relatorio
+#ano
+#empresa
+#página
+#posição (de 1 a x - ordem que aparece na página)
+#"parte" da pagina
+#caracteres
+#sessao (posterior - semi manual)
+#tópicos (posterior - )
+#entidades (posterior - BERT)
+#sentimento (manual?)
+#departamento ? (manual) --> análise qualitativa
+
+#vou precisar dps:
+#um randomizador de parágrafos que me permita tagear as coisas
+#posibilitando dps uma classificação
+#talvez exportar para o qual coder para buscar categorias?
+
+#como vou controlar quais termos foram extraidos?
+#" " " " foram anotados?
+#como inserir colunas a partir das anotações do qualcoder?
+#provavelmente SQLITE
