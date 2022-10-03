166f67415f34505505f5e0582bc0fc3cdc1ee19d
leoaucar <leonardoaucar@gmail.com>
2022-10-03 14:57:07 -0300

    text pre-processing with steeming, lemmetization, post tag

---
+++ NLTK_preprocessing.py
@@ -0,0 +1,67 @@
+# carrega bibliotecas necessárias
+import pandas as pd
+import nltk
+import re
+from nltk.tokenize import word_tokenize
+from nltk import stem
+from nltk.stem import WordNetLemmatizer
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('omw-1.4')
+from nltk.corpus import stopwords
+
+from collections import Counter
+
+
+
+
+def nltk_cleaning(document_df):
+    stopwords = stopwords.words('english')
+
+    report_sentences = document_df['text'].copy()
+
+    df_lems= pd.DataFrame()
+    df_tags = pd.DataFrame()
+
+    lems_list = []
+    tags_list = []
+
+    for sent in report_sentences:
+        #tokenização e deixar em minúsculas
+        lower_words = [word.lower() for word in word_tokenize(sent)]
+        
+        #tratamentos de texto
+        clean_words= (re.sub("[^A-Za-z']+", ' ', str(word)).lower() for word in lower_words)
+        clean_words= (word for word in clean_words if len(word) > 1)
+
+
+        #remover stopwords
+        clean_words = [word for word in clean_words if word not in stopwords]
+        
+        #Stemming
+        stemmer = stem.PorterStemmer()
+        stem_words = [stemmer.stem(word) for word in clean_words]
+        
+        #lemmatização
+        lemmetizer = WordNetLemmatizer()
+        palavras_lem = [lemmetizer.lemmatize(word) for word in stem_words]
+        lems_list.append(palavras_lem)
+        
+        #taggind das palavras
+        tags = [nltk.pos_tag([word]) for word in clean_words]
+        tags_list.append(tags)
+        
+
+    print(lems_list)
+    print(tags_list)
+    df_lems = pd.concat([df_lems,pd.Series(lems_list)])
+    df_tags = pd.concat([df_tags,pd.Series(tags_list)])
+    document_df = pd.concat([document_df,df_lems,df_tags], axis=1)
+
+    #print(palavras_lem)
+    print(df_lems.shape)
+    print(df_tags.shape)
+    print(document_df.shape)
+    document_df = document_df.rename(columns={document_df.columns[-2]:'lems'})
+    document_df = document_df.rename(columns={document_df.columns[-1]:'tags'})
+    return document_df
---
+++ program.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import os
+
+from prepare_csv import concat_new_df, create_table, fill_table, column_types
+from NLTK_preprocessing import nltk_cleaning
+
+
+#prepara um CSV base caso não haja algum
+try:
+    documents_df = pd.read_csv("base.csv") #le o documento consolidado
+except:
+    documents_df = create_table() #caso nao exista, cria esse doc
+    documents_df.to_csv("base.csv", index=False)
+
+
+# identifica o diretorio e lista os documentos a serem lidos
+dir_path = r'./Relatorios_Volkswagen'
+documents_list = [] #pegar lista de documentos
+
+for path in os.listdir(dir_path):
+    # garante que é um arquivo
+    if os.path.isfile(os.path.join(dir_path, path)):
+        documents_list.append(path)
+print("Documentos processados: " + 
+    str(documents_list))
+
+#concatena DF com textos
+for document in documents_list:
+    new_documents_table = fill_table("./Relatorios_Volkswagen/" + document)
+    documents_df = concat_new_df(documents_df, new_documents_table)
+
+#documents_df = column_types(documents_df)
+#documents_df = nltk_cleaning(documents_df)
+
+#salva base processada num CSV
+documents_df.to_csv('base.csv', escapechar="|", index=False)
+
+#recarrega a base completa e aplica pre processamento
+df = pd.read_csv('base.csv')
+new_df = nltk_cleaning(df)
+new_df.to_csv('nltk_base.csv')
+
+
+
+
+
+#loop for vai passar relatório por relatório
+#em cada relatório vai tentar extrair todas as páginas
+#vai adicionar linhas num grande DF (abaixo colunas)
+#ao fim exportaremos o CSV
+
+#id
+#texto (como vou separar títulos? e tabelas?)
+#documento/relatorio
+#ano
+#empresa
+#página
+#posição (de 1 a x - ordem que aparece na página)
+#"parte" da pagina
+#caracteres
+#sessao (posterior - semi manual)
+#tópicos (posterior - )
+#entidades (posterior - BERT)
+#sentimento (manual?)
+#departamento ? (manual) --> análise qualitativa
+
+#vou precisar dps:
+#um randomizador de parágrafos que me permita tagear as coisas
+#posibilitando dps uma classificação
+#talvez exportar para o qual coder para buscar categorias?
+
+#como vou controlar quais termos foram extraidos?
+#" " " " foram anotados?
+#como inserir colunas a partir das anotações do qualcoder?
+#provavelmente SQLITE
